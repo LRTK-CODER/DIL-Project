d298b877b6b3116b3607ed31040475b49f21adf2
LRTK <lrtk@kakao.com>
2021-08-18 08:34:19 +0900

    리펙토링

--- User/LRTK/Code/new/process.py
+++ User/LRTK/Code/new/process.py
@@ -1,41 +1,34 @@
-import pandas, time
+import pandas
 
 DataFrame = pandas.DataFrame
 
-def columnDelete(data:DataFrame, currentList:list):
-    data.drop(currentList, axis=1, inplace=True)
+class DI:
+    def __init__(self, df:DataFrame):
+        self.df = df
 
-def masking(data:DataFrame, column:str, start:int):
-    for index in range(len(data)):
-        data.loc[index, column] = data.loc[index, column][0] + ('*' * (len(data.loc[index, column])-start))
-
-def coding(data:DataFrame, column:str, machingTable:dict):
-    for index in range(len(data)):
-        value = data.loc[index, column]
-        
-        if value in list(machingTable.keys()):
-            data.loc[index, column] = machingTable[data.loc[index, column]]
-
-def rounding(data:DataFrame, column:str):
-    for index in range(len(data)):
-        data.loc[index, column] = round(data.loc[index, column], -1)
-
-def address(data:DataFrame, column:str):
-    for index in range(len(data)):
-        data.loc[index, column] = data.loc[index, column].split()[0]
-
-if __name__ == '__main__':
-    excel = pandas.read_csv('../../Sample/test_100.csv', index_col=0)
+    def columnsDelete(self, columns:list):
+        self.df.drop(columns, axis=1)
     
-    start = time.time()
-    columnDelete(excel, ['회원번호', '생일', '전화번호'])
-    masking(excel, '이름', 1)
-    coding(excel, '성별', {'남성':'m', '여성':'f'})
-    rounding(excel, '나이')
-    address(excel, '주소')
-    print("time :", time.time() - start)
+    def masking(self, column:str):
+        self.df[column] = [data[0]+'**' for data in list(self.df.loc[:, column])]
+    
+    def coding(self, column:str, machingTable:dict):
+        self.df[column] = [machingTable[data] for data in list(self.df.loc[:, column])]
 
-    print(excel.head())
-    # ------------------------------------------------------
-    data = excel.loc[:, ['이름', '성별', '나이', '주소']]
-    data.to_csv('../../Sample/kTest.csv', encoding='utf-8-sig')
+    def rounding(self, column:str):
+        self.df[column] = [round(data, -1) for data in list(self.df.loc[:, column])]
+
+    def address(self, column:str):
+        self.df[column] = [data.split()[0] for data in list(self.df.loc[:, column])]
+            
+if __name__ == '__main__':
+    excel = pandas.read_csv('../../Sample/test.csv', index_col=0)
+    
+    test = DI(excel)
+    test.columnsDelete(['생일', '전화번호'])
+    test.masking('이름')
+    test.coding('성별', {'남성':'M', '여성':'F'})
+    test.rounding('나이')
+    test.address('주소')
+
+    excel.to_csv('../../Sample/kTest_Full.csv', encoding='utf-8-sig') 
