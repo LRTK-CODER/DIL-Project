2918c247e44ba49119aed0f2c2efde1be92d923b
HanSooLim <dlagkstn77@gmail.com>
2021-08-29 11:04:47 +0900

    마스킹, 로컬 일반화, 범주화, 표본 추출 코드 업로드

--- User/hansoolimm/encrypt_create.py
+++
@@ -1,9 +0,0 @@
-import cryption
-
-in_str = input("암호화 할 값을 입력하세요\n")
-
-if in_str != None:
-    in_enc = cryption.MyCipher().encrypt_str(in_str)
-    print(in_enc)
-
-    print("입력된값:", cryption.MyCipher().decrypt_str(in_enc))
