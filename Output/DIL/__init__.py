01ca4e86bc0611d2127ad0fbf1ca822d275b138d
jjy40130 <jjy4013077@gmail.com>
2021-08-29 11:45:58 +0900

    Modify __init__.py

--- Output/DIL/__init__.py
+++ Output/DIL/__init__.py
@@ -1,8 +1,8 @@
-from Aggregation import Aggregation # 총계처리
-from Randomization import Randomization # 무작위화
-from prng import prng # 의사난수
-from twoCrypt import twoCrypt # 양방향 암호화
-from oneCrypt import oneCrypt # 단방향 암호화
+from .Aggregation import Aggregation # 총계처리
+from .Randomization import Randomization # 무작위화
+from .prng import prng # 의사난수
+from .twoCrypt import twoCrypt # 양방향 암호화
+from .oneCrypt import oneCrypt # 단방향 암호화
 
 
 __all__ = ['Aggregation', 'Randomization','prng', 'twoCrypt', 'oneCrypt']
