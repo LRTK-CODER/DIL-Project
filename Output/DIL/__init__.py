6a763fdbf55a14602b76e9cbaccf7dd71a71e1da
jjy40130 <jjy4013077@gmail.com>
2021-09-07 21:36:36 +0900

    Modify /DIL/__init__.py and Renamed OneCry..., TwoCry..., PRNG

--- Output/DIL/__init__.py
+++ Output/DIL/__init__.py
@@ -1,18 +1,18 @@
-from .Aggregation import Aggregation  # 총계처리
+from .Aggregation import Aggregation
 from .Anatomization import Anatomization
 from .Categorization import Categorization
 from .DataRange import DataRange
 from .LocalGeneralization import LocalGeneraliztion
-from .Onecrypt import Onecrypt
+from .OneCrypt import Onecrypt
 from .Permutation import Permutation
 from .PRNG import PRNG
 from .Randomization import Randomization
-# from .Rounding import Round : Class명 수정 요청 - 정재윤
-# from .Sampling import Sampling : Class 정의 요청 - 정재윤
+from .Rounding import Round
+# from .Sampling import Sampling
 from .Suppression import Suppression
-from .TBC import TBC
-from .Twocrypt import Twocrypt
-# from .error import error : Class 밒 파일명 동일 및 첫 문자 대문자 요청 - 정재윤
+# from .TBC import TBC
+from .TwoCrypt import Twocrypt
+# from .error import error
 
 __all__ = ['Aggregation', 'Anatomization', 'Categorization', 'DataRange', 'LocalGeneralization', 'Onecrypt',
            'Permutation', 'PRNG', 'Randomization', 'Suppression', 'TBC', 'Twocrypt']
