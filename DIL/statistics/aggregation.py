c0e8dca9aeafe7a17b381a14ba1f6080738e9dc9
LRTK <lrtk@kakao.com>
2021-09-21 22:56:50 +0900

    총계처리 업로드

---
+++ DIL/statistics/aggregation.py
@@ -0,0 +1,25 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from util import DataSetting
+
+class Aggregation(DataSetting):
+    def mean(self, column:str):
+        meanValue = int(self.datas[column].mean())
+        self.datas[column] = [meanValue] * len(self.datas)
+
+    def max(self, column:str):
+        maxValue = self.datas[column].max()
+        self.datas[column] = [maxValue] * len(self.datas)
+
+    def min(self, column:str):
+        minValue = self.datas[column].min()
+        self.datas[column] = [minValue] * len(self.datas)
+
+    def mode(self, column:str):
+        modeValue = list(self.datas[column].mode())[-1]
+        self.datas[column] = [modeValue] * len(self.datas)
+
+    def median(self, column:str):
+        medianValue = int(self.datas[column].median())
+        self.datas[column] = [medianValue] * len(self.datas)
