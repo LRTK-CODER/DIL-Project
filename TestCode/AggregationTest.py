5334a1a40be20db5e6e1e1fe593940d4633ab0b8
Bae KwonHan <darjeeling@gmail.com>
2021-10-16 21:02:20 +0900

    add pytest.ini *Test.py testing file pattern
    fix absolute file path problem in TestCode/AggregationTest.py

--- TestCode/AggregationTest.py
+++ TestCode/AggregationTest.py
@@ -4,7 +4,12 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import pandas
 from DIL import statistics
 
-excel = pandas.read_csv('../Sample/test_100.csv', index_col=0)
+
+CURRENT_DIR_PATH=os.path.dirname(__file__)
+TEST_FIXTURE_REL_PATH="../Sample/test_100.csv"
+TEST_FIXTURE_PATH=os.path.join(CURRENT_DIR_PATH, TEST_FIXTURE_REL_PATH)
+
+excel = pandas.read_csv(TEST_FIXTURE_PATH, index_col=0)
 print(excel.head())
 
 aggregation = statistics.Aggregation(excel)
