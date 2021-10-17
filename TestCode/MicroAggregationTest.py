98d289e651818e9961176b27f110426605c70a6d
LRTK-CODER <lrtk@kakao.com>
2021-10-17 19:48:31 +0900

    MicroAggregationTest fix

--- TestCode/MicroAggregationTest.py
+++ TestCode/MicroAggregationTest.py
@@ -21,17 +21,62 @@ class TestMicroAggregation:
     def _microAggregationInit(self, microAggregation_fixture):
         self._microAggregation = microAggregation_fixture
 
-    def test_mean(self):
-        mean_value = self._microAggregation.mean('나이', currentIndex=0)
+    @pytest.mark.parametrize('column, currentIndex, result', [
+        ('나이', 0, 46),
+        ('나이', 4, 46),
+        ('나이', 10, 46),
+        ('나이', 23, 46),
+        ('나이', 53, 46)
+    ])
+    def test_mean(self, column, currentIndex, result):
+        mean_value = self._microAggregation.mean(column, currentIndex)
         
-        assert mean_value == 46
+        assert mean_value == result
 
-    def test_max(self):
-        max_value = self._microAggregation.max('나이', currentIndex=0)
+    @pytest.mark.parametrize('column, currentIndex, result', [
+        ('나이', 0, 70),
+        ('나이', 4, 70),
+        ('나이', 10, 70),
+        ('나이', 23, 70),
+        ('나이', 53, 70)
+    ])
+    def test_max(self, column, currentIndex, result):
+        max_value = self._microAggregation.max(column, currentIndex)
         
-        assert max_value == 70
+        assert max_value == result
 
-    def test_median(self):
-        median_value = self._microAggregation.median('나이', currentIndex=0)
+    @pytest.mark.parametrize('column, currentIndex, result', [
+        ('나이', 0, 20),
+        ('나이', 4, 20),
+        ('나이', 10, 20),
+        ('나이', 23, 20),
+        ('나이', 53, 20)
+    ])
+    def test_min(self, column, currentIndex, result):
+        min_value = self._microAggregation.min(column, currentIndex)
+        
+        assert min_value == result
 
-        assert median_value == 45
+    @pytest.mark.parametrize('column, currentIndex, result', [
+        ('나이', 0, 66),
+        ('나이', 4, 66),
+        ('나이', 10, 66),
+        ('나이', 23, 66),
+        ('나이', 53, 66)
+    ])
+    def test_mode(self, column, currentIndex, result):
+        mode_value = self._microAggregation.mode(column, currentIndex)
+        
+        assert mode_value == result
+
+    @pytest.mark.parametrize('column, currentIndex, result', [
+        ('나이', 0, 45),
+        ('나이', 4, 45),
+        ('나이', 10, 45),
+        ('나이', 23, 45),
+        ('나이', 53, 45)
+    ])
+    def test_median(self, column, currentIndex, result):
+        median_value = self._microAggregation.median(column, currentIndex)
+
+        assert median_value == result
