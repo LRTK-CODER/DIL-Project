d48387b118bd623f4b35de991575ea29bf55f860
Bae KwonHan <darjeeling@gmail.com>
2021-10-17 13:32:09 +0900

    AggregationTest fix (#15)
    * add pytest to requirements_dev.txt
    
    * add pytest.ini *Test.py testing file pattern
    
    fix absolute file path problem in TestCode/AggregationTest.py
    
    * fix AggregationTest and aggregation.py to accept test

--- TestCode/AggregationTest.py
+++ TestCode/AggregationTest.py
@@ -4,24 +4,27 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import pandas
 from DIL import statistics
 
-excel = pandas.read_csv('../Sample/test_100.csv', index_col=0)
-print(excel.head())
 
-aggregation = statistics.Aggregation(excel)
+def test_statistics_aggregation():
+    CURRENT_DIR_PATH=os.path.dirname(__file__)
+    TEST_FIXTURE_REL_PATH="../Sample/test_100.csv"
+    TEST_FIXTURE_PATH=os.path.join(CURRENT_DIR_PATH, TEST_FIXTURE_REL_PATH)
+    excel = pandas.read_csv(TEST_FIXTURE_PATH, index_col=0)
+    print(excel.head())
+    aggregation = statistics.Aggregation(excel)
 
-# 평균값
-# aggregation.mean('나이')
+    # 평균값
+    # aggregation.mean('나이')
+    # 최댓값
+    # aggregation.max('나이')
+    # 최솟값
+    # aggregation.min('나이')
+    # 최빈값
+    # aggregation.mode('나이')
+    # 중간값
+    median_value = aggregation.median('나이')
 
-# 최댓값
-# aggregation.max('나이')
-
-# 최솟값
-# aggregation.min('나이')
-
-# 최빈값
-# aggregation.mode('나이')
-
-# 중간값
-aggregation.median('나이')
-
-print(excel.head())
+    # I think 45 is median value from the fixture field '나이'
+    for value in median_value:
+        assert value == 45
+    print(excel.head())
