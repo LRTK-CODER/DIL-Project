5079d3127ae99713992168d0b517d1983af1211c
HanSooLim <dlagkstn77@gmail.com>
2021-10-17 15:26:28 +0900

    test code error

--- DIL/Error.py
+++ DIL/Error.py
@@ -1,3 +1,8 @@
+def test_statistics_aggregation():
+    CURRENT_DIR_PATH=os.path.dirname(__file__)
+    TEST_FIXTURE_REL_PATH="../Sample/test_100.csv"
+    TEST_FIXTURE_PATH=os.path.join(CURRENT_DIR_PATH, TEST_FIXTURE_REL_PATH)
+    
 class Error(Exception):
     def __init__(self, msg:str):
         super().__init__(msg)
