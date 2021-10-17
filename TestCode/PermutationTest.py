bc8cbd47c45e106f4700439d19e4556bf5fab152
LRTK-CODER <lrtk@kakao.com>
2021-10-18 02:22:53 +0900

    Test Class add

--- TestCode/PermutationTest.py
+++ TestCode/PermutationTest.py
@@ -1,15 +1,35 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-import pandas
+import pandas, pytest
 from DIL import randomization
 
-excel = pandas.read_csv('../Sample/test_100.csv', index_col=0)
-print(excel.head())
+# 경로 설정
+CURRENT_DIR_PATH=os.path.dirname(__file__)
+TEST_FIXTURE_REL_PATH="../Sample/test_100.csv"
+TEST_FIXTURE_PATH=os.path.join(CURRENT_DIR_PATH, TEST_FIXTURE_REL_PATH)
 
-permutationTest = randomization.Permutation(excel)
+@pytest.fixture
+def permutation_fixture():
+    excel = pandas.read_csv(TEST_FIXTURE_PATH, index_col=0)
+    dataSetting = randomization.Permutation(excel.copy())
+    
+    return dataSetting
+
+class TestPermutation:
+    @pytest.fixture(autouse=True)
+    def _permutationInit(self, permutation_fixture):
+        self._permutation = permutation_fixture
+        self._currentDatas = permutation_fixture
+
+    def test_dataSetting(self):
+        assert type(self._permutation) is pandas.DataFrame
+
+    def test_all(self):
+        print(self._permutation.all('나이').head())
+        print(self._currentDatas.datas.head())
 
 # 전체 순열
-permutationTest.all('나이')
+# permutationTest.all('나이')
 
-print(excel.head())
+# print(excel.head())
