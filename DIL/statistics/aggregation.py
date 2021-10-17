3c6be03c8edeee42f9101a31dc9c0efa91c83d73
LRTK-CODER <lrtk@kakao.com>
2021-10-17 18:24:06 +0900

    method return add

--- DIL/statistics/aggregation.py
+++ DIL/statistics/aggregation.py
@@ -7,18 +7,23 @@ class Aggregation(DataSetting):
     def mean(self, column:str):
         meanValue = int(self.datas[column].mean())
         self.datas[column] = [meanValue] * len(self.datas)
+        return self.datas[column]
 
     def max(self, column:str):
         maxValue = self.datas[column].max()
         self.datas[column] = [maxValue] * len(self.datas)
+        return self.datas[column]
 
     def min(self, column:str):
         minValue = self.datas[column].min()
         self.datas[column] = [minValue] * len(self.datas)
+        return self.datas[column]
+
 
     def mode(self, column:str):
         modeValue = list(self.datas[column].mode())[-1]
         self.datas[column] = [modeValue] * len(self.datas)
+        return self.datas[column]
 
     def median(self, column:str):
         medianValue = int(self.datas[column].median())
