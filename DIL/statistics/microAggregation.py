e1df80d69b8cbfec800ab28321cb78da1e15dd6a
LRTK-CODER <lrtk@kakao.com>
2021-10-17 19:11:36 +0900

    method return add

--- DIL/statistics/microAggregation.py
+++ DIL/statistics/microAggregation.py
@@ -7,19 +7,24 @@ class MicroAggregation(DataSetting):
     def mean(self, column:str, currentIndex:int):
         meanValue = int(self.datas[column].mean())
         self.datas.at[currentIndex, column] = meanValue
+        return self.datas[column]
 
     def max(self, column:str, currentIndex:int):
         maxValue = self.datas[column].max()
         self.datas.at[currentIndex, column] = maxValue
+        return self.datas[column]
 
     def min(self, column:str, currentIndex:int):
         minValue = self.datas[column].min()
         self.datas.at[currentIndex, column] = minValue
+        return self.datas[column]
 
     def mode(self, column:str, currentIndex:int):
         modeValue = list(self.datas[column].mode())[-1]
         self.datas.at[currentIndex, column] = modeValue
+        return self.datas[column]
 
     def median(self, column:str, currentIndex:int):
         medianValue = int(self.datas[column].median())
         self.datas.at[currentIndex, column] = medianValue
+        return self.datas[column]
