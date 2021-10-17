7f522d5156fdb5b2f4d4f8a805c2ec556209b41a
HanSooLim <dlagkstn77@gmail.com>
2021-10-17 19:24:11 +0900

    Modify AggregationTest.py

--- TestCode/AggregationTest.py
+++ TestCode/AggregationTest.py
@@ -18,7 +18,7 @@ def aggregation_fixture():
 
 class TestAggregation:
     @pytest.fixture(autouse=True)
-    def _aggregationInit(self, agrregation_fixture):
+    def _aggregationInit(self, aggregation_fixture):
         self._aggregation = aggregation_fixture
 
     def test_mean(self):
@@ -26,6 +26,24 @@ class TestAggregation:
 
         for value in mean_value:
             assert value == 46
+    
+    def test_max(self):
+        max_value = self._aggregation.max('나이')
+
+        for value in max_value:
+            assert value == 70
+
+    def test_min(self):
+        min_value = self._aggregation.min('나이')
+
+        for value in min_value:
+            assert value == 20
+
+    def test_mode(self):
+        mode_value = self._aggregation.mode('나이')
+
+        for value in mode_value:
+            assert value == 66
 
     def test_median(self):
         median_value = self._aggregation.median('나이')
