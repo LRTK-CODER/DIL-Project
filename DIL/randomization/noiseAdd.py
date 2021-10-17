dbe02ce2450238c6229b91dfd49c737336fa8fc7
LRTK-CODER <lrtk@kakao.com>
2021-10-17 23:44:59 +0900

    Method return add

--- DIL/randomization/noiseAdd.py
+++ DIL/randomization/noiseAdd.py
@@ -13,9 +13,12 @@ class Noise(DataSetting):
         result = [data + random.randrange(randomRange[0], randomRange[1]) for data in datas]
         
         self.datas[column] = result
+        return self.datas[column]
 
     def multipleAdd(self, columns:list, randomRange:list=[-9, 9]):
         noiseList = [random.randrange(randomRange[0], randomRange[1]) for _ in range(0, len(self.datas))]
         for idx, noise in enumerate(noiseList):
             for column in columns:
                 self.datas.at[idx, column] += noise
+
+        return self.datas[columns]
