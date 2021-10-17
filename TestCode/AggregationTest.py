4d1a3f80a6d62128c729e1c6a2fa89bf587c83c9
LRTK-CODER <lrtk@kakao.com>
2021-10-17 18:19:11 +0900

    TestAggregation class create

--- TestCode/AggregationTest.py
+++ TestCode/AggregationTest.py
@@ -1,7 +1,7 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-import pandas
+import pandas, pytest
 from DIL import statistics
 
 # 경로 설정
@@ -9,16 +9,21 @@ CURRENT_DIR_PATH=os.path.dirname(__file__)
 TEST_FIXTURE_REL_PATH="../Sample/test_100.csv"
 TEST_FIXTURE_PATH=os.path.join(CURRENT_DIR_PATH, TEST_FIXTURE_REL_PATH)
 
-class TestStatisticsAggregation:
+@pytest.fixture
+def agrregation_fixture():
+    excel = pandas.read_csv(TEST_FIXTURE_PATH, index_col=0)
+    dataSetting = statistics.Aggregation(excel.copy())
+    
+    return dataSetting
 
-    def test_statistics_aggregation_median_value():
-        excel = pandas.read_csv(TEST_FIXTURE_PATH, index_col=0)
-        print(excel.head())
-        aggregation = statistics.Aggregation(excel)
+class TestAggregation:
+    @pytest.fixture(autouse=True)
+    def _agrregationInit(self, agrregation_fixture):
+        self._agrregation = agrregation_fixture
 
-        median_value = aggregation.median('나이')
+    def test_median(self):
+        median_value = self._agrregation.median('나이')
 
         # I think 45 is median value from the fixture field '나이'
         for value in median_value:
             assert value == 45
-        print(excel.head())
