eebd4bb0c4adf67cd9bf87ea7377f3a99cdae922
LRTK-CODER <lrtk@kakao.com>
2021-10-18 01:49:54 +0900

    NoiseAddTest fix

--- TestCode/NoiseAddTest.py
+++ TestCode/NoiseAddTest.py
@@ -1,18 +1,45 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-import pandas
+import pandas, pytest
 from DIL import randomization
 
-excel = pandas.read_csv('../Sample/test_100.csv', index_col=0)
-print(excel.head())
+# 경로 설정
+CURRENT_DIR_PATH=os.path.dirname(__file__)
+TEST_FIXTURE_REL_PATH="../Sample/test_100.csv"
+TEST_FIXTURE_PATH=os.path.join(CURRENT_DIR_PATH, TEST_FIXTURE_REL_PATH)
 
-noiseTest = randomization.Noise(excel)
+@pytest.fixture
+def noise_fixture():
+    excel = pandas.read_csv(TEST_FIXTURE_PATH, index_col=0)
+    dataSetting = randomization.Noise(excel)
+    
+    return dataSetting
 
-# 컬럼 1개 잡음 추가
-# noiseTest.add('나이')
+class TestNoise:
+    @pytest.fixture(autouse=True)
+    def _noiseInit(self, noise_fixture):
+        self._noise = noise_fixture
 
-# 관련된 컬럼 잡음 추가
-noiseTest.multipleAdd(['회원번호', '나이'])
+    def test_dataSetting(self):
+        assert type(self._noise.datas) is pandas.DataFrame
 
-print(excel.head())
+    @pytest.mark.parametrize('column, randomRange', [
+        ('나이', [-9, 9]),
+        ('나이', [-8, 9]),
+        ('나이', [-8, 8]),
+        ('나이', [-7, 8]),
+    ])
+    def test_add(self, column, randomRange):
+        noiseAdd_Value = self._noise.add(column, randomRange)
+        assert not noiseAdd_Value.equals(noise_fixture)
+    
+    @pytest.mark.parametrize('column, randomRange', [
+        (['회원번호', '나이'], [-9, 9]),
+        (['회원번호', '나이'], [-8, 9]),
+        (['회원번호', '나이'], [-8, 8]),
+        (['회원번호', '나이'], [-7, 8]),
+    ])
+    def test_mulipleAdd(self, column, randomRange):
+        noiseAdd_Value = self._noise.multipleAdd(column, randomRange)
+        assert not noiseAdd_Value.equals(noise_fixture)
