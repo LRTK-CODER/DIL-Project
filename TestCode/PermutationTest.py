5da0d8711df23223a8c2ccb79a73ad5ee83f8174
LRTK-CODER <lrtk@kakao.com>
2021-10-18 14:18:21 +0900

    PermutationTest all method test

--- TestCode/PermutationTest.py
+++ TestCode/PermutationTest.py
@@ -20,16 +20,12 @@ class TestPermutation:
     @pytest.fixture(autouse=True)
     def _permutationInit(self, permutation_fixture):
         self._permutation = permutation_fixture
-        self._currentDatas = permutation_fixture
 
     def test_dataSetting(self):
-        assert type(self._permutation) is pandas.DataFrame
+        assert type(self._permutation.datas) is pandas.DataFrame
 
     def test_all(self):
-        print(self._permutation.all('나이').head())
-        print(self._currentDatas.datas.head())
+        permutation_value = self._permutation.all('나이')
+        excel = pandas.read_csv(TEST_FIXTURE_PATH, index_col=0)
 
-# 전체 순열
-# permutationTest.all('나이')
-
-# print(excel.head())
+        assert not permutation_value.equals(excel['나이'])
