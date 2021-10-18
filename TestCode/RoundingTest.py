53b7ac302fe82ff8fcfc057088165a2e8c0698fa
LRTK-CODER <lrtk@kakao.com>
2021-10-18 16:26:50 +0900

    Rounding Test fix

--- TestCode/RoundingTest.py
+++ TestCode/RoundingTest.py
@@ -1,22 +1,60 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-import pandas
+import pandas, pytest
 from DIL import Rounding
 
-excel = pandas.read_csv('../Sample/test_100.csv', index_col=0)
-print(excel.head())
+# 경로 설정
+CURRENT_DIR_PATH=os.path.dirname(__file__)
+TEST_FIXTURE_REL_PATH="../Sample/test_100.csv"
+TEST_FIXTURE_PATH=os.path.join(CURRENT_DIR_PATH, TEST_FIXTURE_REL_PATH)
 
-# 반올림
-# excel['나이'] = [Rounding.off(data, 1) for data in list(excel['나이'])]
+excel = pandas.read_csv(TEST_FIXTURE_PATH, index_col=0)
 
-# 올림
-# excel['나이'] = [Rounding.up(data, 1) for data in list(excel['나이'])]
+class TestRound:
+    @pytest.mark.parametrize('data, seatNum, result', [
+        (4, 1, 0),
+        (10, 1, 10),
+        (25, 1, 30),
+        (12, 1, 10),
+        (49, 1, 50),
+        (150, 2, 200),
+        (141, 2, 100)
+    ])
+    def test_off(self, data, seatNum, result):
+        rounding_off_value = Rounding.off(data, seatNum)
 
-# 내림
-# excel['나이'] = [Rounding.down(data, 1) for data in list(excel['나이'])]
+        assert rounding_off_value == result
 
-# 랜덤 라운딩
-Rounding.random(excel, '나이')
+    @pytest.mark.parametrize('data, seatNum, result', [
+        (10, 1, 10),
+        (25, 1, 30),
+        (12, 1, 20),
+        (49, 1, 50),
+        (150, 2, 200),
+        (141, 2, 200)
+    ])
+    def test_up(self, data, seatNum, result):
+        rounding_up_value = Rounding.up(data, seatNum)
 
-print(excel.head())
+        assert rounding_up_value == result
+
+    @pytest.mark.parametrize('data, seatNum, result', [
+        (10, 1, 10),
+        (25, 1, 20),
+        (12, 1, 10),
+        (49, 1, 40),
+        (150, 2, 100),
+        (141, 2, 100)
+    ])
+    def test_down(self, data, seatNum, result):
+        rounding_down_value = Rounding.down(data, seatNum)
+
+        assert rounding_down_value == result
+
+    def test_random(self):
+        excel = pandas.read_csv(TEST_FIXTURE_PATH, index_col=0)
+        datas = Rounding.random(excel, '나이')
+
+        for data in datas:
+            assert str(data)[-1] == '0'
