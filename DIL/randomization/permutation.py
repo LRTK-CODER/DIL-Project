f5a14f748f3585d19174cf494e7125e3f817fc75
LRTK-CODER <lrtk@kakao.com>
2021-10-18 14:17:19 +0900

    permutation all method fix

--- DIL/randomization/permutation.py
+++ DIL/randomization/permutation.py
@@ -1,13 +1,13 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-import random
+import random, numpy as np, pandas
 from util import DataSetting
 
 class Permutation(DataSetting):
     def all(self, column:str):
-        datas = self._toList(column)
-        random.shuffle(datas)
+        datas = self.datas.loc[:, [column]]
+        shuttleDatas = datas.sample(frac=1).reset_index(drop=True)
         
-        self.datas[column] = datas
+        self.datas[column] = shuttleDatas
         return self.datas[column]
