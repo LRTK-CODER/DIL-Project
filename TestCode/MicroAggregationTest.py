557e013422f206370fd63706469fd2436224d4de
LRTK <lrtk@kakao.com>
2021-10-20 13:39:19 +0900

    conftest csv path fixture add

--- TestCode/MicroAggregationTest.py
+++ TestCode/MicroAggregationTest.py
@@ -1,81 +1,64 @@
 import sys, os
+
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 import pandas, pytest
 from DIL import statistics
 
-# 경로 설정
-CURRENT_DIR_PATH=os.path.dirname(__file__)
-TEST_FIXTURE_REL_PATH="../Sample/test_100.csv"
-TEST_FIXTURE_PATH=os.path.join(CURRENT_DIR_PATH, TEST_FIXTURE_REL_PATH)
 
 @pytest.fixture
-def microAggregation_fixture():
-    excel = pandas.read_csv(TEST_FIXTURE_PATH, index_col=0)
+def microAggregation_fixture(path_fixture):
+    excel = pandas.read_csv(path_fixture, index_col=0)
     dataSetting = statistics.MicroAggregation(excel)
-    
+
     return dataSetting
+
 
 class TestMicroAggregation:
     @pytest.fixture(autouse=True)
     def _microAggregationInit(self, microAggregation_fixture):
         self._microAggregation = microAggregation_fixture
 
-    @pytest.mark.parametrize('column, currentIndex, result', [
-        ('나이', 0, 46),
-        ('나이', 4, 46),
-        ('나이', 10, 46),
-        ('나이', 23, 46),
-        ('나이', 53, 46)
-    ])
+    @pytest.mark.parametrize(
+        "column, currentIndex, result",
+        [("나이", 0, 46), ("나이", 4, 46), ("나이", 10, 46), ("나이", 23, 46), ("나이", 53, 46)],
+    )
     def test_mean(self, column, currentIndex, result):
         mean_value = self._microAggregation.mean(column, currentIndex)
-        
+
         assert mean_value == result
 
-    @pytest.mark.parametrize('column, currentIndex, result', [
-        ('나이', 0, 70),
-        ('나이', 4, 70),
-        ('나이', 10, 70),
-        ('나이', 23, 70),
-        ('나이', 53, 70)
-    ])
+    @pytest.mark.parametrize(
+        "column, currentIndex, result",
+        [("나이", 0, 70), ("나이", 4, 70), ("나이", 10, 70), ("나이", 23, 70), ("나이", 53, 70)],
+    )
     def test_max(self, column, currentIndex, result):
         max_value = self._microAggregation.max(column, currentIndex)
-        
+
         assert max_value == result
 
-    @pytest.mark.parametrize('column, currentIndex, result', [
-        ('나이', 0, 20),
-        ('나이', 4, 20),
-        ('나이', 10, 20),
-        ('나이', 23, 20),
-        ('나이', 53, 20)
-    ])
+    @pytest.mark.parametrize(
+        "column, currentIndex, result",
+        [("나이", 0, 20), ("나이", 4, 20), ("나이", 10, 20), ("나이", 23, 20), ("나이", 53, 20)],
+    )
     def test_min(self, column, currentIndex, result):
         min_value = self._microAggregation.min(column, currentIndex)
-        
+
         assert min_value == result
 
-    @pytest.mark.parametrize('column, currentIndex, result', [
-        ('나이', 0, 66),
-        ('나이', 4, 66),
-        ('나이', 10, 66),
-        ('나이', 23, 66),
-        ('나이', 53, 66)
-    ])
+    @pytest.mark.parametrize(
+        "column, currentIndex, result",
+        [("나이", 0, 66), ("나이", 4, 66), ("나이", 10, 66), ("나이", 23, 66), ("나이", 53, 66)],
+    )
     def test_mode(self, column, currentIndex, result):
         mode_value = self._microAggregation.mode(column, currentIndex)
-        
+
         assert mode_value == result
 
-    @pytest.mark.parametrize('column, currentIndex, result', [
-        ('나이', 0, 45),
-        ('나이', 4, 45),
-        ('나이', 10, 45),
-        ('나이', 23, 45),
-        ('나이', 53, 45)
-    ])
+    @pytest.mark.parametrize(
+        "column, currentIndex, result",
+        [("나이", 0, 45), ("나이", 4, 45), ("나이", 10, 45), ("나이", 23, 45), ("나이", 53, 45)],
+    )
     def test_median(self, column, currentIndex, result):
         median_value = self._microAggregation.median(column, currentIndex)
 
