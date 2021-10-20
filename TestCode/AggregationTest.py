efbe5bbc7bebabeb619741636bf03903ed11247e
LRTK <lrtk@kakao.com>
2021-10-20 13:38:04 +0900

    conftest csv pash fixture add

--- TestCode/AggregationTest.py
+++ TestCode/AggregationTest.py
@@ -1,20 +1,18 @@
 import sys, os
+
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 import pandas, pytest
 from DIL import statistics
 
-# 경로 설정
-CURRENT_DIR_PATH=os.path.dirname(__file__)
-TEST_FIXTURE_REL_PATH="../Sample/test_100.csv"
-TEST_FIXTURE_PATH=os.path.join(CURRENT_DIR_PATH, TEST_FIXTURE_REL_PATH)
 
 @pytest.fixture
-def aggregation_fixture():
-    excel = pandas.read_csv(TEST_FIXTURE_PATH, index_col=0)
+def aggregation_fixture(path_fixture):
+    excel = pandas.read_csv(path_fixture, index_col=0)
     dataSetting = statistics.Aggregation(excel.copy())
-    
+
     return dataSetting
+
 
 class TestAggregation:
     @pytest.fixture(autouse=True)
@@ -22,31 +20,31 @@ class TestAggregation:
         self._aggregation = aggregation_fixture
 
     def test_mean(self):
-        mean_value = self._aggregation.mean('나이')
+        mean_value = self._aggregation.mean("나이")
 
         for value in mean_value:
             assert value == 46
-    
+
     def test_max(self):
-        max_value = self._aggregation.max('나이')
+        max_value = self._aggregation.max("나이")
 
         for value in max_value:
             assert value == 70
 
     def test_min(self):
-        min_value = self._aggregation.min('나이')
+        min_value = self._aggregation.min("나이")
 
         for value in min_value:
             assert value == 20
 
     def test_mode(self):
-        mode_value = self._aggregation.mode('나이')
+        mode_value = self._aggregation.mode("나이")
 
         for value in mode_value:
             assert value == 66
 
     def test_median(self):
-        median_value = self._aggregation.median('나이')
+        median_value = self._aggregation.median("나이")
 
         # I think 45 is median value from the fixture field '나이'
         for value in median_value:
