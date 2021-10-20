d396546c775409158b0c3ecf82b66a1d3c40e733
LRTK <lrtk@kakao.com>
2021-10-20 13:44:16 +0900

    conftest csv path fixture add

--- TestCode/PermutationTest.py
+++ TestCode/PermutationTest.py
@@ -1,20 +1,18 @@
 import sys, os
+
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 import pandas, pytest
 from DIL import randomization
 
-# 경로 설정
-CURRENT_DIR_PATH=os.path.dirname(__file__)
-TEST_FIXTURE_REL_PATH="../Sample/test_100.csv"
-TEST_FIXTURE_PATH=os.path.join(CURRENT_DIR_PATH, TEST_FIXTURE_REL_PATH)
 
 @pytest.fixture
-def permutation_fixture():
-    excel = pandas.read_csv(TEST_FIXTURE_PATH, index_col=0)
+def permutation_fixture(path_fixture):
+    excel = pandas.read_csv(path_fixture, index_col=0)
     dataSetting = randomization.Permutation(excel.copy())
-    
+
     return dataSetting
+
 
 class TestPermutation:
     @pytest.fixture(autouse=True)
@@ -25,7 +23,7 @@ class TestPermutation:
         assert type(self._permutation.datas) is pandas.DataFrame
 
     def test_all(self):
-        permutation_value = self._permutation.all('나이')
+        permutation_value = self._permutation.all("나이")
         excel = pandas.read_csv(TEST_FIXTURE_PATH, index_col=0)
 
-        assert not permutation_value.equals(excel['나이'])
+        assert not permutation_value.equals(excel["나이"])
