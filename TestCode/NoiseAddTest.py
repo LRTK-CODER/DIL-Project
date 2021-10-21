e2ecaed1895d090b48e23cd384142b1422923be1
LRTK <lrtk@kakao.com>
2021-10-21 13:16:43 +0900

    Pytest conftest add (#33)
    * conftest create csv path fixture
    
    * conftest csv pash fixture add
    
    * conftest csv path fixture add
    
    * conftest csv path fixture add
    
    * conftest csv path fixture add
    
    * Permutation Test code fix
    
    * github action fix
    
    * github action fix - TestCode directory move
    
    * pytest conftest.py root directory move
    
    * github action Run python test fix
    
    * Permutation Test Code Fix

--- TestCode/NoiseAddTest.py
+++ TestCode/NoiseAddTest.py
@@ -1,20 +1,18 @@
 import sys, os
+
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 import pandas, pytest
 from DIL import randomization
 
-# 경로 설정
-CURRENT_DIR_PATH=os.path.dirname(__file__)
-TEST_FIXTURE_REL_PATH="../Sample/test_100.csv"
-TEST_FIXTURE_PATH=os.path.join(CURRENT_DIR_PATH, TEST_FIXTURE_REL_PATH)
 
 @pytest.fixture
-def noise_fixture():
-    excel = pandas.read_csv(TEST_FIXTURE_PATH, index_col=0)
+def noise_fixture(path_fixture):
+    excel = pandas.read_csv(path_fixture, index_col=0)
     dataSetting = randomization.Noise(excel)
-    
+
     return dataSetting
+
 
 class TestNoise:
     @pytest.fixture(autouse=True)
@@ -24,22 +22,28 @@ class TestNoise:
     def test_dataSetting(self):
         assert type(self._noise.datas) is pandas.DataFrame
 
-    @pytest.mark.parametrize('column, randomRange', [
-        ('나이', [-9, 9]),
-        ('나이', [-8, 9]),
-        ('나이', [-8, 8]),
-        ('나이', [-7, 8]),
-    ])
+    @pytest.mark.parametrize(
+        "column, randomRange",
+        [
+            ("나이", [-9, 9]),
+            ("나이", [-8, 9]),
+            ("나이", [-8, 8]),
+            ("나이", [-7, 8]),
+        ],
+    )
     def test_add(self, column, randomRange):
         noiseAdd_Value = self._noise.add(column, randomRange)
         assert not noiseAdd_Value.equals(noise_fixture)
-    
-    @pytest.mark.parametrize('column, randomRange', [
-        (['회원번호', '나이'], [-9, 9]),
-        (['회원번호', '나이'], [-8, 9]),
-        (['회원번호', '나이'], [-8, 8]),
-        (['회원번호', '나이'], [-7, 8]),
-    ])
+
+    @pytest.mark.parametrize(
+        "column, randomRange",
+        [
+            (["회원번호", "나이"], [-9, 9]),
+            (["회원번호", "나이"], [-8, 9]),
+            (["회원번호", "나이"], [-8, 8]),
+            (["회원번호", "나이"], [-7, 8]),
+        ],
+    )
     def test_mulipleAdd(self, column, randomRange):
         noiseAdd_Value = self._noise.multipleAdd(column, randomRange)
         assert not noiseAdd_Value.equals(noise_fixture)
