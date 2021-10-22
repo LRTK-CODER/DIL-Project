3b43bf564901414adf21ad11bb6c853171474bda
LRTK <lrtk@kakao.com>
2021-10-22 09:47:51 +0900

    python black formatting

--- DIL/etc.py
+++ DIL/etc.py
@@ -3,14 +3,20 @@ from .util import DataSetting
 
 DataFrame = pandas.DataFrame
 
+
 class Etc(DataSetting):
-    def sampling(self, percent:int) -> DataFrame:
-        datas = self.datas.loc[:,].sample(n=len(self.datas)//percent)
+    def sampling(self, percent: int) -> DataFrame:
+        datas = self.datas.loc[
+            :,
+        ].sample(n=len(self.datas) // percent)
         return datas
 
-    def anatomization(self, identyColumn:str, currentColumnList:list) -> DataFrame:
+    def anatomization(self, identyColumn: str, currentColumnList: list) -> DataFrame:
         currentColumnList.insert(0, identyColumn)
         nonCurrentColumnList = list(set(list(self.datas)) - set(currentColumnList))
         nonCurrentColumnList.insert(0, identyColumn)
-        
-        return self.datas.loc[:, currentColumnList], self.datas.loc[:, nonCurrentColumnList]
+
+        return (
+            self.datas.loc[:, currentColumnList],
+            self.datas.loc[:, nonCurrentColumnList],
+        )
