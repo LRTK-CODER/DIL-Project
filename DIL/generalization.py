9fe4939c540a432f04c47089bca9c067b683e070
LRTK <lrtk@kakao.com>
2021-10-22 11:40:18 +0900

    Modify generalization test (#39)
    * local method fix
    
    * local method test fix
    
    * categorizion test fix
    
    * Github action test target TestCode directory modify
    
    * _generalization variable modify

--- DIL/generalization.py
+++ DIL/generalization.py
@@ -1,11 +1,14 @@
 from .util import DataSetting
 
-class Generalization(DataSetting):
-    def local(self, column:str, currentIndexList:list) -> None:
-        datas = self.datas.loc[currentIndexList[0]:currentIndexList[1], column]
-        self.datas.loc[currentIndexList[0]:currentIndexList[1], column] = [f'{min(datas)}~{max(datas)}' for _ in datas]
 
-    def categorizion(self, column:str, replaceList:list, category:str) -> None:
+class Generalization(DataSetting):
+    def local(self, column: str, currentIndexList: list) -> None:
+        datas = self.datas.loc[currentIndexList[0] : currentIndexList[1], column]
+        self.datas.loc[currentIndexList[0] : currentIndexList[1], column] = [
+            f"{min(datas)} ~ {max(datas)}" for _ in datas
+        ]
+
+    def categorizion(self, column: str, replaceList: list, category: str) -> None:
         datas = self._toList(column)
         result = [category if data in replaceList else data for data in datas]
         self.datas.loc[:, column] = result
--- TestCode/generalizationTest.py
+++ TestCode/generalizationTest.py
@@ -1,18 +1,63 @@
-import sys, os
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-
-import pandas
+import pandas, pytest
 from DIL import Generalization
 
-excel = pandas.read_csv('../Sample/test_100.csv', index_col=0)
-print(excel.head())
 
-genTest = Generalization(excel)
+@pytest.fixture
+def generalization_fixture(datas_fixture):
+    dataSetting = Generalization(datas_fixture.copy())
 
-# 로컬 일반화
-# genTest.local(column='성별', currentIndexList=[0, 3])
+    return dataSetting
 
-# 문자데이터 범주화
-genTest.categorizion(column='성별', replaceList=['남성', '여성'], category='Gender')
 
-print(excel.head())
+class TestGeneralization:
+    @pytest.fixture(autouse=True)
+    def _generalizationInit(self, generalization_fixture):
+        self._generalization = generalization_fixture
+
+    @pytest.mark.parametrize(
+        "local_scope",
+        [
+            [0, 3],
+            [1, 4],
+            [5, 100],
+            [2, 3],
+            [20, 30],
+        ],
+    )
+    def test_local_target(self, local_scope):
+        targetColumn = "성별"
+        self._generalization.local(column=targetColumn, currentIndexList=local_scope)
+
+        local_values = self._generalization.datas
+        for value in local_values[targetColumn][local_scope[0] : local_scope[1] + 1]:
+            assert value == "남성 ~ 여성"
+
+    @pytest.mark.parametrize(
+        "local_scope",
+        [
+            [0, 3],
+            [1, 4],
+            [5, 100],
+            [2, 3],
+            [20, 30],
+        ],
+    )
+    def test_local_non_target(self, local_scope):
+        targetColumn = "성별"
+        self._generalization.local(column=targetColumn, currentIndexList=local_scope)
+
+        local_values = self._generalization.datas
+        for value in local_values[targetColumn][local_scope[1] + 1 :]:
+            assert value != "남성 ~ 여성"
+
+    def test_categorizion(self):
+        targetColumn = "성별"
+        category = "Gender"
+
+        self._generalization.categorizion(
+            column=targetColumn, replaceList=["남성", "여성"], category=category
+        )
+
+        categorizion_values = self._generalization.datas
+        for value in categorizion_values[targetColumn]:
+            assert value == category
