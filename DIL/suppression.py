e1664dac84ba7537efbc765942ef114f42a99904
LRTK <lrtk@kakao.com>
2021-10-22 09:22:10 +0900

    Modify suppression test (#37)
    * general method return fix
    
    * suppression general test fix
    
    * partial return fix
    
    * partial test fix
    
    * record method return fix
    
    * record test fix
    
    * masking method return fix
    
    * masking test fix
    
    * address method return fix
    
    * address test fix
    
    * Github action test code suppression add

--- DIL/suppression.py
+++ DIL/suppression.py
@@ -1,85 +1,94 @@
 from .util import DataSetting
 
+
 class Suppression(DataSetting):
     """
-        Creator : 오석재 & 임한수
-        Data : 2021.09.01
-        
-        [Explanation]
-            삭제 기술(일반 삭제, 부분 삭제, 행 항목 삭제, 마스킹)을 구현한 클래스
-            모든 메소드는 생성자에 원본 데이터를 인자 값으로 넣으면 원본 데이터를 수정한다. 그래서 반환 값이 없음.
+    Creator : 오석재 & 임한수
+    Data : 2021.09.01
+
+    [Explanation]
+        삭제 기술(일반 삭제, 부분 삭제, 행 항목 삭제, 마스킹)을 구현한 클래스
+        모든 메소드는 생성자에 원본 데이터를 인자 값으로 넣으면 원본 데이터를 수정한다. 그래서 반환 값이 없음.
     """
+
     def __intCheck(self, datas):
-        if datas.dtype == 'int64':
+        if datas.dtype == "int64":
             return True
         return False
 
-    def general(self, columns:list=None) -> None:
+    def general(self, columns: list = None) -> None:
         for col in columns:
             del self.datas[col]
 
-    def partial(self, column:str, scope:list) -> None:
+        return True
+
+    def partial(self, column: str, scope: list) -> None:
         datas = self.datas[column]
-        
+
         # int형 타입 검사 및 str 변환
         intFlag = False
         if self.__intCheck(datas):
-            datas = datas.astype('str')
+            datas = datas.astype("str")
             intFlag = True
 
         result = list()
         for data in datas:
             data = list(data)
-            del data[scope[0]:scope[1]+1]
-            
-            result.append(''.join(data))
+            del data[scope[0] : scope[1] + 1]
+
+            result.append("".join(data))
 
         if intFlag:
-           result = list(map(int, result))
+            result = list(map(int, result))
 
         self.datas[column] = result
+        return self.datas[column]
 
-    def record(self, currentIndexList:list) -> None:
+    def record(self, currentIndexList: list) -> None:
         self.datas.drop(currentIndexList, axis=0, inplace=True)
+        return self.datas
 
-    def local(self, column:str, currentIndexList:list) -> None:        
+    def local(self, column: str, currentIndexList: list) -> None:
         datas = self._toList(column)
-        
+
         for idx in currentIndexList:
-            datas[idx] = ''
-        
+            datas[idx] = ""
+
         self.datas[column] = datas
 
-    def masking(self, column:str, scope:list) -> None:
+    def masking(self, column: str, scope: list) -> None:
         datas = self.datas[column]
-        
+
         # int형 타입 검사 및 str 변환
         if self.__intCheck(datas):
-            datas = datas.astype('str')
+            datas = datas.astype("str")
 
         result = list()
-        for data in datas:            
+        for data in datas:
             data = list(data)
-            data[scope[0]:scope[1]] = '*' * (scope[1] - scope[0])
+            data[scope[0] : scope[1]] = "*" * (scope[1] - scope[0])
 
-            result.append(''.join(data))
+            result.append("".join(data))
 
         self.datas[column] = result
+        return self.datas[column]
 
-    def address(self, column:str, mode:int=2):
+    def address(self, column: str, mode: int = 2):
         if mode == 1:
             self.__stateRearDel(column)
         else:
             self.__cityRearDel(column)
 
-    def __stateRearDel(self, column:str):
+        return self.datas[column]
+
+    def __stateRearDel(self, column: str):
         datas = self._toList(column)
-        
+
         result = [data.split()[0] for data in datas]
         self.datas[column] = result
-    
-    def __cityRearDel(self, column:str):
+
+    def __cityRearDel(self, column: str):
         datas = self._toList(column)
-        
-        result = [' '.join(data.split()[:2]) for data in datas]
+
+        result = [" ".join(data.split()[:2]) for data in datas]
         self.datas[column] = result
--- TestCode/SuppressionTest.py
+++ TestCode/SuppressionTest.py
@@ -1,33 +1,77 @@
-import sys, os
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-
-import pandas
+import pandas, pytest
 from DIL import Suppression
 
-excel = pandas.read_csv('../Sample/test_100.csv', index_col=0)
-print(excel.head())
 
-suppressionTest = Suppression(excel)
+@pytest.fixture
+def suppression_fixture(datas_fixture):
+    dataSetting = Suppression(datas_fixture.copy())
 
-# 일반 삭제
-# suppressionTest.general(['전화번호', '주소'])
+    return dataSetting
 
-# 부분 삭제
-# suppressionTest.partial('이름', [1, 2])
 
-# 레코드 삭제
-# suppressionTest.record([0])
-# suppressionTest.record([0, 2])
+class TestSuppression:
+    @pytest.fixture(autouse=True)
+    def _suppressionInit(self, suppression_fixture):
+        self._suppression = suppression_fixture
 
-# 로컬 삭제
-# suppressionTest.local('이름', [0])
-# suppressionTest.local('이름', [0, 2])
+    def test_general(self):
+        origeralColumn = list(self._suppression.datas)
+        currentColumn = ["전화번호", "주소"]
 
-# 마스킹
-# suppressionTest.masking('이름', [1, 3])
+        self._suppression.general(["전화번호", "주소"])
 
-# 주소 부분 삭제
-# suppressionTest.address('주소', 1)
-suppressionTest.address('주소', 2)
+        assert sorted(list(set(origeralColumn) - set(currentColumn))) == sorted(
+            list(self._suppression.datas)
+        )
 
-print(excel.head())
+    def test_partial(self):
+        targetColumn = "이름"
+        original = self._suppression.datas[targetColumn].copy()
+
+        for original_value, partial_value in zip(
+            original,
+            self._suppression.partial(targetColumn, [1, 2]),
+        ):
+            assert original_value != partial_value
+
+    @pytest.mark.parametrize(
+        "currentIndexList",
+        [
+            ([0]),
+            ([0, 2]),
+            ([2, 6, 8]),
+            ([10, 39, 70, 80]),
+            ([10, 2, 70, 80, 99]),
+            ([1, 0, 3, 10, 25, 56, 43]),
+        ],
+    )
+    def test_record(self, currentIndexList):
+        original_length = len(self._suppression.datas)
+        delete_count = len(currentIndexList)
+
+        record_values = self._suppression.record(currentIndexList)
+
+        assert original_length - delete_count == len(record_values)
+
+    def test_masking(self):
+        targetColumn = "이름"
+        masking_scope = [1, 3]
+
+        masking_values = self._suppression.masking(targetColumn, masking_scope)
+
+        for value in masking_values:
+            assert value.count("*") == masking_scope[1] - masking_scope[0]
+
+    def test_address_state(self):
+        original = self._suppression.datas.copy()
+        address_values = self._suppression.address("주소", 1)
+
+        for original_value, address_value in zip(original, address_values):
+            assert original_value != address_value
+
+    def test_address_city(self):
+        original = self._suppression.datas.copy()
+        address_values = self._suppression.address("주소", 2)
+
+        for original_value, address_value in zip(original, address_values):
+            assert original_value != address_value
