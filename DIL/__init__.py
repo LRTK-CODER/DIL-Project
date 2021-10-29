d88d082dd4a5290bb26105f9c65d8e8f466eb79d
LRTK <lrtk@kakao.com>
2021-10-29 13:35:54 +0900

    DIL __init__ modify

--- DIL/__init__.py
+++ DIL/__init__.py
@@ -1,6 +1,15 @@
+from .randomization import __init__
+from .statistics import __init__
+from .crypto import __init__
+from .util import __init__
 from .suppression import Suppression
 from .rounding import Rounding
 from .generalization import Generalization
 from .etc import Etc
 
-__all__ = ["Suppression", "Rounding", "Generalization", "Etc"]
+__all__ = [
+    "Suppression",
+    "Rounding",
+    "Generalization",
+    "Etc",
+]
