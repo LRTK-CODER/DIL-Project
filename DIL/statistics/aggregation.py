358d60d96b1e5e86d3c95bb1a942c81bf8398554
LRTK <lrtk@kakao.com>
2021-11-03 18:48:44 +0900

    sphinx api doc comment (#54)
    * sphinx api doc comment
    
    * TestCode Modify

--- DIL/statistics/aggregation.py
+++ DIL/statistics/aggregation.py
@@ -6,27 +6,102 @@ from util import DataSetting
 
 
 class Aggregation(DataSetting):
+    """
+    | 통계 기술 중 총계 기술(평균값, 최댓값, 최솟값, 최빈값, 중간값)을 구현한 클래스
+    | 모든 메소드는 생성자에 원본 데이터를 인자 값으로 넣으면 원본 데이터를 수정한다.
+    """
+
     def mean(self, column: str):
+        """
+        총계 기술의 평균값을 구현한 메소드
+
+        Parameters
+        ----------
+        column : str
+            총계 기술의 평균값을 적용할 컬럼
+
+        Returns
+        -------
+        True
+            기술 적용 성공 시 True 리턴
+        """
         meanValue = int(self.datas[column].mean())
         self.datas[column] = [meanValue] * len(self.datas)
-        return self.datas[column]
+
+        return True
 
     def max(self, column: str):
+        """
+        총계 기술의 최댓값을 구현한 메소드
+
+        Parameters
+        ----------
+        column : str
+            총계 기술의 최댓값을 적용할 컬럼
+
+        Returns
+        -------
+        True
+            기술 적용 성공 시 True 리턴
+        """
         maxValue = self.datas[column].max()
         self.datas[column] = [maxValue] * len(self.datas)
-        return self.datas[column]
+
+        return True
 
     def min(self, column: str):
+        """
+        총계 기술의 최솟값을 구현한 메소드
+
+        Parameters
+        ----------
+        column : str
+            총계 기술의 최솟값을 적용할 컬럼
+
+        Returns
+        -------
+        True
+            기술 적용 성공 시 True 리턴
+        """
         minValue = self.datas[column].min()
         self.datas[column] = [minValue] * len(self.datas)
-        return self.datas[column]
+
+        return True
 
     def mode(self, column: str):
+        """
+        총계 기술의 최빈값을 구현한 메소드
+
+        Parameters
+        ----------
+        column : str
+            총계 기술의 최빈값을 적용할 컬럼
+
+        Returns
+        -------
+        True
+            기술 적용 성공 시 True 리턴
+        """
         modeValue = list(self.datas[column].mode())[-1]
         self.datas[column] = [modeValue] * len(self.datas)
-        return self.datas[column]
+
+        return True
 
     def median(self, column: str):
+        """
+        총계 기술의 중간값을 구현한 메소드
+
+        Parameters
+        ----------
+        column : str
+            총계 기술의 중간값을 적용할 컬럼
+
+        Returns
+        -------
+        True
+            기술 적용 성공 시 True 리턴
+        """
         medianValue = int(self.datas[column].median())
         self.datas[column] = [medianValue] * len(self.datas)
-        return self.datas[column]
+
+        return True
--- TestCode/AggregationTest.py
+++ TestCode/AggregationTest.py
@@ -15,31 +15,36 @@ class TestAggregation:
         self._aggregation = aggregation_fixture
 
     def test_mean(self):
-        mean_value = self._aggregation.mean("나이")
+        self._aggregation.mean("나이")
+        mean_value = self._aggregation.datas["나이"]
 
         for value in mean_value:
             assert value == 46
 
     def test_max(self):
-        max_value = self._aggregation.max("나이")
+        self._aggregation.max("나이")
+        max_value = self._aggregation.datas["나이"]
 
         for value in max_value:
             assert value == 70
 
     def test_min(self):
-        min_value = self._aggregation.min("나이")
+        self._aggregation.min("나이")
+        min_value = self._aggregation.datas["나이"]
 
         for value in min_value:
             assert value == 20
 
     def test_mode(self):
-        mode_value = self._aggregation.mode("나이")
+        self._aggregation.mode("나이")
+        mode_value = self._aggregation.datas["나이"]
 
         for value in mode_value:
             assert value == 66
 
     def test_median(self):
-        median_value = self._aggregation.median("나이")
+        self._aggregation.median("나이")
+        median_value = self._aggregation.datas["나이"]
 
         # I think 45 is median value from the fixture field '나이'
         for value in median_value:
