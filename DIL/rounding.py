25cd25c2b707a8545a5eee90ede5a611c890374f
LRTK <lrtk@kakao.com>
2021-11-03 21:12:51 +0900

    sphinx api doc comment

--- DIL/rounding.py
+++ DIL/rounding.py
@@ -4,8 +4,12 @@ DataFrame = pandas.DataFrame
 
 
 class Rounding:
+    """
+    라운딩 기술(일반 라운딩, 랜덤 라운딩)을 구현한 클래스
+    """
+
     @staticmethod
-    def off(data: int, seatNum: int) -> int:
+    def off(data: int, seatNum: int):
         """
         라운딩 기술 중 반올림을 수행하는 메소드
 
@@ -19,7 +23,7 @@ class Rounding:
         Returns
         -------
         int type data
-            기술 적용 성공 시 True 리턴
+            반올림이 적용된 숫자형 타입 데이터를 리턴
         """
         data = list(str(data))
         if int(data[-seatNum]) >= 5:
@@ -31,7 +35,22 @@ class Rounding:
         return int("".join(data))
 
     @staticmethod
-    def up(data: int, seatNum: int) -> int:
+    def up(data: int, seatNum: int):
+        """
+        라운딩 기술 중 올림을 수행하는 메소드
+
+        Parameters
+        ----------
+        data : int
+            올림을 수행할 숫자형 타입 데이터
+        seatNum : int
+            올림을 수행할 숫자형 타입 데이터의 자리수 위치
+
+        Returns
+        -------
+        int type data
+            올림이 적용된 숫자형 타입 데이터를 리턴
+        """
         data = list(str(data))
         if int(data[-seatNum]) or len(set(data[-seatNum:])) > 1:
             data[-seatNum - 1] = str(int(data[-seatNum - 1]) + 1)
@@ -40,7 +59,22 @@ class Rounding:
         return int("".join(data))
 
     @staticmethod
-    def down(data: int, seatNum: int) -> int:
+    def down(data: int, seatNum: int):
+        """
+        라운딩 기술 중 내림을 수행하는 메소드
+
+        Parameters
+        ----------
+        data : int
+            내림을 수행할 숫자형 타입 데이터
+        seatNum : int
+            내림을 수행할 숫자형 타입 데이터의 자리수 위치
+
+        Returns
+        -------
+        int type data
+            내림이 적용된 숫자형 타입 데이터를 리턴
+        """
         data = list(str(data))
         if int(data[-seatNum]) or len(set(data[-seatNum:])) > 1:
             data[-seatNum:] = "0" * len(data[-seatNum:])
@@ -48,7 +82,22 @@ class Rounding:
         return int("".join(data))
 
     @staticmethod
-    def random(df: DataFrame, column: str) -> None:
+    def random(df: DataFrame, column: str):
+        """
+        라운딩 기술 중 무작위로 수행하는 메소드
+
+        Parameters
+        ----------
+        data : int
+            무작위로 수행할 숫자형 타입 데이터
+        seatNum : int
+            무작위로 수행할 숫자형 타입 데이터의 자리수 위치
+
+        Returns
+        -------
+        True
+            기술 적용 성공 시 True 리턴
+        """
         datas = df.loc[:, column]
 
         result = []
@@ -58,4 +107,4 @@ class Rounding:
 
         df.loc[:, column] = result
 
-        return df[column]
+        return True
