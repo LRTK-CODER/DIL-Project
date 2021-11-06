49e7f1d58ba03741a23d5dd0419750ddcbc8c565
LRTK <lrtk@kakao.com>
2021-11-06 21:24:37 +0900

    Sphinx api doc comment

--- DIL/suppression.py
+++ DIL/suppression.py
@@ -6,26 +6,19 @@ class Suppression(DataSetting):
     | 삭제 기술(일반 삭제, 부분 삭제, 행 항목 삭제, 마스킹)을 구현한 클래스
     | 모든 메소드는 생성자에 원본 데이터를 인자 값으로 넣으면 원본 데이터를 수정한다.
 
-    Parameters
-    ----------
-        - datas : pandas.DataFrame
-            삭제 기술을 적용할 DataFrame 지정
+    Args:
+        datas (pandas.DataFrame) : 삭제 기술을 적용할 DataFrame 지정
     """
 
     def __intCheck(self, datas):
         """
         기술 적용하고자 하는 DataFrame이 'int64 타입'으로 되어있는지 확인하는 함수
 
-        Parameters
-        ----------
-            - datas : pandas.DataFrame
-                int64 타입 검사를 진행할 DataFrame
+        Args:
+            datas (pandas.DataFrame) : int64 타입 검사를 진행할 DataFrame
 
-        Returns
-        -------
-            - True or False
-                | int64 타입이면 True 리턴
-                | int64 타입이 아니면 False 리턴
+        Returns:
+            bool : int64 타입이면 True 리턴, int64 타입이 아니면 False 리턴
         """
         if datas.dtype == "int64":
             return True
@@ -35,15 +28,11 @@ class Suppression(DataSetting):
         """
         삭제 기술 중 일반 삭제를 수행하는 함수
 
-        Parameters
-        ----------
-            - columns : list
-                일반 삭제를 적용할 컬럼들
+        Args:
+            columns (list) : 일반 삭제를 적용할 컬럼들
 
-        Returns
-        -------
-            - True
-                기술 적용 성공 시 True 리턴
+        Returns:
+            bool : 기술 적용 성공 시 True 리턴
         """
         for col in columns:
             del self.datas[col]
@@ -54,17 +43,12 @@ class Suppression(DataSetting):
         """
         삭제 기술 중 부분 삭제를 수행하는 함수
 
-        Parameters
-        ----------
-            - column : str
-                부분 삭제를 적용할 컬럼
-            - scope : list
-                데이터의 부분 삭제 적용 범위 지정
+        Args:
+            column (str) : 부분 삭제를 적용할 컬럼
+            scope (list) : 데이터의 부분 삭제 적용 범위 지정
 
-        Returns
-        -------
-            - True
-                기술 적용 성공 시 True 리턴
+        Returns:
+            bool : 기술 적용 성공 시 True 리턴
         """
         datas = self.datas[column]
 
@@ -92,15 +76,11 @@ class Suppression(DataSetting):
         """
         삭제 기술 중 행 항목 삭제를 수행하는 함수
 
-        Parameters
-        ----------
-            - currentIndexList : list
-                삭제 할 Index list
+        Args:
+            currentIndexList (list) : 삭제 할 Index list
 
-        Returns
-        -------
-            - True
-                기술 적용 성공 시 True 리턴
+        Returns:
+            bool : 기술 적용 성공 시 True 리턴
         """
         self.datas.drop(currentIndexList, axis=0, inplace=True)
 
@@ -110,17 +90,12 @@ class Suppression(DataSetting):
         """
         삭제 기술 중 로컬 삭제를 수행하는 함수
 
-        Parameters
-        ----------
-            - column : str
-                로컬 삭제 기술을 적용할 컬럼
-            - currentIndexList : list
-                로컬 삭제 기술을 적용할 Index list
+        Args:
+            column (str) : 로컬 삭제 기술을 적용할 컬럼
+            currentIndexList (list) : 로컬 삭제 기술을 적용할 Index list
 
-        Returns
-        -------
-            - True
-                기술 적용 성공 시 True 리턴
+        Returns:
+            bool : 기술 적용 성공 시 True 리턴
         """
         datas = self._toList(column)
 
@@ -135,17 +110,12 @@ class Suppression(DataSetting):
         """
         삭제 기술 중 마스킹을 수행하는 함수
 
-        Parameters
-        ----------
-            - column : str
-                마스킹 기술을 적용할 컬럼
-            - scope : list
-                데이터에 마스킹 기술을 적용할 범위 지정
+        Args:
+            column (str) : 마스킹 기술을 적용할 컬럼
+            scope (list) : 데이터에 마스킹 기술을 적용할 범위 지정
 
-        Returns
-        -------
-            - True
-                기술 적용 성공 시 True 리턴
+        Returns:
+            bool : 기술 적용 성공 시 True 리턴
         """
         datas = self.datas[column]
 
@@ -168,19 +138,15 @@ class Suppression(DataSetting):
         """
         주소 부분 삭제를 수행하는 함수
 
-        Parameters
-        ----------
-            - column : str
-                주소 부분 삭제를 적용할 컬럼
-            - mode: int
+        Args:
+            column (str) : 주소 부분 삭제를 적용할 컬럼
+            mode (int) :
                 | 값이 1일 때, 도 단위까지 부분 삭제
                 | 값이 2일 때, 시 단위까지 부분 삭제
                 | Default = 2
 
-        Returns
-        -------
-            - True
-                기술 적용 성공 시 True 리턴
+        Returns:
+            bool : 기술 적용 성공 시 True 리턴
         """
         if mode == 1:
             self.__stateRearDel(column)
